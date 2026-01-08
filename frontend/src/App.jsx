826929cdea2a6a4a841f2f9db6d32a22e15305e0
bhaleraorushi1213 <bhaleraorushi1213@gmail.com>
2026-01-08 16:57:46 +0530

    signup page frontend implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,46 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-	const { authuser, login, isLoggedIn } = useAuthStore();
+	const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
 
-	console.log("authuser", authuser);
-	console.log("isLoggedIn", isLoggedIn);
+	useEffect(() => {
+		checkAuth();
+	}, [checkAuth]);
+
+	if (isCheckingAuth) return <PageLoader />;
 
 	return (
 		<div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
 			{/* DECORATORS - GRID BG & GLOW SHAPES */}
 
-      <div className="absolute inset-0 pointer-events-none">
-        <div className="absolute inset-0 bg-[radial-gradient(circle_at_1px_1px,rgba(255,255,255,0.08)_1px,transparent_0)] bg-[size:24px_24px]" />
-      </div>
+			<div className="absolute inset-0 pointer-events-none">
+				<div className="absolute inset-0 bg-[radial-gradient(circle_at_1px_1px,rgba(255,255,255,0.08)_1px,transparent_0)] bg-[size:24px_24px]" />
+			</div>
 
-			<button className="btn btn-primary" onClick={login}>login</button>
-      
 			<Routes>
-				<Route path="/" element={<ChatPage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/signup" element={<SignUpPage />} />
+				<Route
+					path="/"
+					element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+				/>
+				<Route
+					path="/login"
+					element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+				/>
+				<Route
+					path="/signup"
+					element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+				/>
 			</Routes>
+
+			<Toaster />
 		</div>
 	);
 }
